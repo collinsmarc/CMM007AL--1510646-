d237c3b65a970ec2d9aca51bcefdd3e884051edc
collinsmarc <m.a.collins@rgu.ac.uk>
2016-05-04 07:30:23 +0100

    hello

--- Test.php
+++ Test.php
@@ -1,5 +1,5 @@
 <?php
-echo"<p> Does this work?</p>
+echo"<p> Does this work? test123</p>
 
 <h1>Hello ECHO!!!!!!!</h1>";
 
---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+define ('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define ('DB_USERNAME','b4f37ec3582045');
+define('DB_PASSWORD', 'd2510593');
+define('DB_DATABASE', 'CMM007ALDB-1510646');
+
+$db=mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+?>
